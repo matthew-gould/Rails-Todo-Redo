a0ee7d2695d0bab129112f7901d4045340328ee1
matthew gould <gouldm44@gmail.com>
2015-03-03 12:19:45 -0500

    Initial commit with model mogrations

---
+++ db/migrate/20150303171928_create_items.rb
@@ -0,0 +1,13 @@
+class CreateItems < ActiveRecord::Migration
+  def change
+    create_table :items do |t|
+      t.string, :name
+      t.string, :due_date
+      t.boolean, :completed
+      t.string :default
+      t.string :false
+
+      t.timestamps null: false
+    end
+  end
+end
